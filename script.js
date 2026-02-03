d88846cec751631d5d1199c86fb3a2633fed9cad
aswinr05 <aswinr22052004@gmail.com>
2026-02-03 16:12:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.getElementById("travelForm").addEventListener("submit", function (e) {
     preferences: document.getElementById("preferences").value
   };
 
-  const webhookURL = "https://aswinr005.app.n8n.cloud/webhook/aswin";
+  const webhookURL = "https://aswinr005.app.n8n.cloud/webhook/f3cd2a08-f211-4609-9563-fe00a4f68ec9";
 
   fetch(webhookURL, {
     method: "POST",
